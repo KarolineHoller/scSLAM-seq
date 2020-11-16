94830472476d51dfe82f3be831fc1c47705cfc09
Karoline Holler <63735546+KarolineHoller@users.noreply.github.com>
2020-11-16 13:25:38 +0100

    Update scSLAMseq_danioRerio_6hpf.R

--- scSLAMseq_danioRerio_6hpf.R
+++ scSLAMseq_danioRerio_6hpf.R
@@ -1,11 +1,64 @@
-#mitochondrial genes in dr11 names ####
+##this script has been run on a system with the following specifications: ####
+#R version 3.6.0 (2019-04-26)
+#Platform: x86_64-redhat-linux-gnu (64-bit)
+#Running under: CentOS Linux 7 (Core)
+
+#Matrix products: default
+#BLAS/LAPACK: /usr/lib64/R/lib/libRblas.so
+
+#locale:
+# [1] LC_CTYPE=en_US.UTF-8       LC_NUMERIC=C               LC_TIME=en_US.UTF-8        LC_COLLATE=en_US.UTF-8    
+# [5] LC_MONETARY=en_US.UTF-8    LC_MESSAGES=en_US.UTF-8    LC_PAPER=en_US.UTF-8       LC_NAME=C                 
+# [9] LC_ADDRESS=C               LC_TELEPHONE=C             LC_MEASUREMENT=en_US.UTF-8 LC_IDENTIFICATION=C       
+
+#attached base packages:
+#[1] grid      stats     graphics  grDevices utils     datasets  methods   base     
+
+#other attached packages:
+# [1] deconvolveR_1.1   mixtools_1.2.0    gridExtra_2.3     cowplot_1.0.0     ggrepel_0.8.1     reshape2_1.4.3   
+# [7] viridis_0.5.1     viridisLite_0.3.0 Seurat_3.1.2      seqinr_3.6-1      ggpubr_0.3.0      ggplot2_3.2.1    
+
+#loaded via a namespace (and not attached):
+#  [1] TH.data_1.0-10      Rtsne_0.15          colorspace_1.4-1    ggsignif_0.6.0      rio_0.5.16          ellipsis_0.3.0     
+#  [7] ggridges_0.5.1      leiden_0.3.1        listenv_0.8.0       npsurv_0.4-0        mvtnorm_1.0-11      codetools_0.2-16   
+# [13] splines_3.6.0       R.methodsS3_1.7.1   mnormt_1.5-5        lsei_1.2-0          TFisher_0.2.0       ade4_1.7-15        
+# [19] jsonlite_1.6        broom_0.5.6         ica_1.0-2           kernlab_0.9-29      cluster_2.0.8       png_0.1-7          
+# [25] R.oo_1.23.0         uwot_0.1.5          sctransform_0.2.1   compiler_3.6.0      httr_1.4.1          backports_1.1.5    
+# [31] assertthat_0.2.1    Matrix_1.2-17       lazyeval_0.2.2      htmltools_0.4.0     tools_3.6.0         rsvd_1.0.2         
+# [37] igraph_1.2.4.2      gtable_0.3.0        glue_1.3.1          RANN_2.6.1          dplyr_0.8.3         rappdirs_0.3.1     
+# [43] Rcpp_1.0.3          carData_3.0-4       Biobase_2.44.0      cellranger_1.1.0    vctrs_0.3.0         multtest_2.40.0    
+# [49] gdata_2.18.0        ape_5.3             nlme_3.1-139        gbRd_0.4-11         lmtest_0.9-37       stringr_1.4.0      
+# [55] globals_0.12.5      openxlsx_4.1.5      lifecycle_0.2.0     irlba_2.3.3         gtools_3.8.1        rstatix_0.5.0      
+# [61] future_1.15.1       MASS_7.3-51.4       zoo_1.8-6           scales_1.1.0        hms_0.5.3           parallel_3.6.0     
+# [67] sandwich_2.5-1      RColorBrewer_1.1-2  curl_4.3            reticulate_1.14     pbapply_1.4-2       segmented_1.1-0    
+# [73] stringi_1.4.3       mutoss_0.1-12       plotrix_3.7-7       caTools_1.17.1.3    BiocGenerics_0.30.0 zip_2.0.4          
+# [79] bibtex_0.4.2.2      Rdpack_0.11-1       SDMTools_1.1-221.2  rlang_0.4.6         pkgconfig_2.0.3     bitops_1.0-6       
+# [85] lattice_0.20-38     ROCR_1.0-7          purrr_0.3.3         htmlwidgets_1.5.1   tidyselect_1.1.0    RcppAnnoy_0.0.14   
+# [91] plyr_1.8.5          magrittr_1.5        R6_2.4.1            generics_0.0.2      gplots_3.0.1.1      multcomp_1.4-11    
+# [97] haven_2.3.0         foreign_0.8-71      pillar_1.4.3        withr_2.1.2         sn_1.5-4            fitdistrplus_1.0-14
+#[103] abind_1.4-5         survival_2.44-1.1   tibble_3.0.1        future.apply_1.3.0  tsne_0.1-3          car_3.0-8          
+#[109] crayon_1.3.4        KernSmooth_2.23-15  plotly_4.9.1        readxl_1.3.1        data.table_1.12.8   forcats_0.5.0      
+#[115] metap_1.2           digest_0.6.23       tidyr_1.0.0         numDeriv_2016.8-1.1 R.utils_2.9.2       RcppParallel_4.4.4 
+#[121] stats4_3.6.0        munsell_0.5.0      
+
+##load environment ####
+library(Seurat)
+library(viridis)
+library(ggplot2)
+library(reshape2)
+library(ggrepel)
+library(cowplot)
+library(grid)
+library(gridExtra)
+
+#mitochondrial genes in with annotation of dR11 ####
 #that file sits on the server in my local/genomes
-mito.genes <- read.table("/local/users/aneusch/genomes/mito.genes.vs.txt",sep = ",")
+mito.genes <- read.table("/your/file/path/mito.genes.vs.txt",sep = ",")
 mito.genes <- mito.genes$V3
 mito.genes <- as.character(mito.genes)
 
 #prepare seurat-object for the unlabeled data
-unlabeled <- Read10X(data.dir = paste0("/local/users/aneusch/experiments/10x/20190306_AB/workdir/20190820_cellranger3_labeled_unlabeled_seurat/remap_unlabeled_Q20/Solo.out_/"))
+unlabeled <- Read10X(data.dir = paste0("/your/file/path/Solo.out_/"))
 unlabeled <- CreateSeuratObject(counts = unlabeled,
                                 min.cells = 3, min.features = 150,
                                 project = "unlabeled")
@@ -28,7 +81,7 @@ unlabeled <- FindNeighbors(unlabeled, reduction = "pca", dims = 1:20)
 unlabeled <- FindClusters(unlabeled, resolution = 0.5)
 
 ## prepare Seurat-Object for labeled reads ####
-labeled <- Read10X(data.dir = paste0("/local/users/aneusch/experiments/10x/20190306_AB/workdir/20190820_cellranger3_labeled_unlabeled_seurat/remap_labeled_Q20/Solo.out_/"))
+labeled <- Read10X(data.dir = paste0("/your/file/path/Solo.out_/"))
 labeled <- CreateSeuratObject(counts = labeled,
                               min.cells = 3, min.features = 150,
                               project = "labeled")
@@ -94,7 +147,7 @@ unlabeled.markers.lc <- FindAllMarkers(object = unlabeled, only.pos = TRUE)
 
 #cluster 7 marker genes as a dataframe
 cluster7 <- unlabeled.markers.lc[unlabeled.markers.lc$cluster==7,]
-veg.tomoseq <- read.csv("/local/Karo/scSLAM/6h/vegetal.genes.dR.csv", header=T)
+veg.tomoseq <- read.csv("/your/file/path/vegetal.genes.dR.csv", header=T)
 
 g=intersect(veg.tomoseq$Gene, cluster7$gene)
 length(g) #28 - the overlap between vegetally localized genes and cluster 7 marker genes ('prospective PGCs')
